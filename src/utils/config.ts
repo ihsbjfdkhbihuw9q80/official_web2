84cf24ef8a72c26ee1ed93206e0346448bc5b0c0
ihsbjfdkhbihuw9q80 <87159106+ihsbjfdkhbihuw9q80@users.noreply.github.com>
2025-03-17 23:13:44 +0800

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 import type { TemplateConfig } from "./configType";
 
 const templateConfig: TemplateConfig = {
-  name: "Lorem Ipsum",
+  name: "Oasis",
   seo: {
     title: "Mobile App Landing Template",
     description: "Mobile App Landing Template",
@@ -31,7 +31,6 @@ const templateConfig: TemplateConfig = {
     links: [
       { href: "/#features", title: "Features" },
       { href: "/#how-it-works", title: "How it works" },
-      { href: "/#pricing", title: "Pricing" },
       { href: "/#faq", title: "FAQ" },
     ],
   },
@@ -43,7 +42,6 @@ const templateConfig: TemplateConfig = {
     links: [
       { href: "/#features", title: "Features" },
       { href: "/#how-it-works", title: "How it works" },
-      { href: "/#pricing", title: "Pricing" },
       { href: "/#faq", title: "FAQ" },
     ],
   },
@@ -218,42 +216,6 @@ const templateConfig: TemplateConfig = {
       usersDescription: "100+ people already using the app",
       headlineMark: [1, 3],
     },
-    pricing: {
-      id: "pricing",
-      title: "Pricing",
-      subtitle: "Flexible costs to meet your budget",
-      actionText: "Download the app",
-      plans: [
-        {
-          title: "Basic Plan",
-          price: "$9.99/month",
-          rows: ["Access to basic features", "Email support", "1 GB storage"],
-        },
-        {
-          title: "Standard Plan",
-          price: "$19.99/month",
-          featured: true,
-          rows: [
-            "Access to all basic features",
-            "Priority email support",
-            "10 GB storage",
-            "Monthly webinars",
-          ],
-        },
-        {
-          title: "Premium Plan",
-          price: "$29.99/month",
-          rows: [
-            "Access to all features",
-            "24/7 support",
-            "100 GB storage",
-            "Weekly webinars",
-            "Exclusive content",
-          ],
-        },
-      ],
-    },
-  },
   privacyPolicy: {
     seo: {
       title: "Privacy Policy - Mobile App Landing Template",
